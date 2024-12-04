4c1f51e310cc3cb2b869459edd44b48438fc008c
Bovet Gauthier <gauthier.bovet@icloud.com>
2024-12-04 19:51:51 +0100

    Fin de taff solo

--- TankInvadersV0/GameClass/Entity.py
+++ TankInvadersV0/GameClass/Entity.py
@@ -4,16 +4,14 @@ from tkinter import *
 class Entities:
     
     def __init__(self, canvas, pos, img, hp, size, proj, fproj):
-        
-        self.pos = pos
-        self.img = PhotoImage(file = img)
-        self.image_id = None
         self.canvas = canvas
-
-        self.hp = hp
-        self.size = size
-        self.proj = proj
-        self.fproj = fproj
+        self.pos = pos  #Position sous forme de coordonnées avec x,y
+        self.img = PhotoImage(file = img)   #Image associée à un objet
+        self.image_id = None
+        self.hp = hp    #Les points de vie associés à l'objet
+        self.size = size    #Taille de l'objet (x=largeur et y=hauteur)
+        self.proj = proj    #Les projectiles que peut lancer ou non un objet
+        self.fproj = fproj  #La fréquence de tir des projectiles 
         
     def show(self):
         if self.image_id is None:
