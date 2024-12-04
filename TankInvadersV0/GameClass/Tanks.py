81b137bf9c7fd1f9016720ba6f5d90c7cb91d16d
yanisDrx <yanis.pires2004@gmail.com>
2024-12-04 18:35:55 +0100

    normalement c'est le dernier

--- TankInvadersV0/GameClass/Tanks.py
+++ TankInvadersV0/GameClass/Tanks.py
@@ -7,8 +7,4 @@ class Tank(Entities):
     def __init__(self, canvas, pos, img, hp, size, proj, fproj):
         super().__init__(canvas, pos, img, hp, size, proj, fproj)
         
-        # pil_img = Image.open(img)
-        # pil_img = pil_img.resize((pil_img.width * 2, pil_img.height * 2))
-        
-        
         self.show()
