6c50f809bd82d5476f76d8fbf2ab73f971551fa2
yanisDrx <yanis.pires2004@gmail.com>
2024-12-04 19:49:57 +0100

    animation du tank

--- TankInvadersV0/GameClass/Entity.py
+++ TankInvadersV0/GameClass/Entity.py
@@ -5,16 +5,17 @@ class Entities:
     
     def __init__(self, canvas, pos, img, hp, size, proj, fproj):
         
+        self.canvas = canvas
         self.pos = pos
         self.img = PhotoImage(file = img)
         self.image_id = None
-        self.canvas = canvas
-
+        
         self.hp = hp
         self.size = size
         self.proj = proj
         self.fproj = fproj
         
     def show(self):
+        
         if self.image_id is None:
             self.image_id = self.canvas.create_image(self.pos[0], self.pos[1], image=self.img)
--- TankInvadersV0/GameClass/Soldat.py
+++ TankInvadersV0/GameClass/Soldat.py
@@ -13,10 +13,10 @@ class Soldat(Entities):
     def move(self, dx):
         """Déplace le soldat horizontalement."""
         self.pos[0] = self.pos[0] + dx 
-        if self.pos[0] < 10:
+        if self.pos[0] <= 10:
             self.pos[0] = 10
             dx = 0
-        if self.pos[0] > 790:
+        if self.pos[0] >= 790:
             self.pos[0] = 790
             dx = 0
         if self.image_id is not None:  # Vérifier si l'image est bien créée
